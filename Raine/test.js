78cc091bdfbd400ff13690706b1c19948cb63e59
Raine Low <rainelow48@gmail.com>
2022-01-23 11:02:10 +0000

    attempt to save bootpage

--- Raine/test.js
+++ Raine/test.js
@@ -1,10 +1,34 @@
 // Different dictionaries for different purposes
-symbols = { 'equals to': '=', 'plus': '+', 'minus': '-', 'times': '* ', 'divide': '/' }
+symbols = { 'equals to': '=', 'equals': '=', 'equal': '=', 'plus': '+', 'minus': '-', 'times': '* ', 'divide': '/', 'divided': '/' }
 brackets = { 'open bracket': '(', 'close bracket': ')' }
-
-// List of dictionaries
-dictionaries = [symbols, brackets]
-
+// Caps greek 'Α  , Β , Γ , Δ , Ε , Ζ , Η , Θ , Ι , Κ , Λ , Μ , Ν , Ξ , Ο , Π , Ρ , Σ /ς, Τ , Υ , Φ , Χ , Ψ , and Ω '
+greeks = {
+    'alpha': 'α',
+    'beta': 'β',
+    'gamma': 'γ',
+    'delta': 'δ',
+    'epsilon': 'ε',
+    'zeta': 'ζ',
+    'eta': 'η',
+    'theta': 'θ',
+    'iota': 'ι',
+    'kappa': 'κ',
+    'lambda': 'λ',
+    'mu': 'μ',
+    'nu': 'ν',
+    'xi': 'ξ',
+    'omicron': 'ο',
+    'pi': 'π',
+    'rho': 'ρ',
+    'sigma': 'σ',
+    'tau': 'τ',
+    'upsilon': 'υ',
+    'phi': 'φ',
+    'chi': 'χ',
+    'psi': 'ψ',
+    'omega': 'ω',
+    'hello': 'hi'
+};
 
 function mergeDict(dictionaries) {
     newDict = {}
@@ -15,22 +39,38 @@ function mergeDict(dictionaries) {
     return newDict
 }
 
-function speechReplace(transcript, dictionaries) {
-    // Iterate through different dictionaries in the dictionary list
-    for (i = 0; i < dictionaries.length; i++) {
-        dictName = dictionaries[i]
-        keys = Object.keys(dictName)
-        for (j = 0; j < keys.length; j++) {
-            transcript = transcript.replaceAll(keys[j], dictName[keys[j]])
-        }
+function speechReplace(transcript, newDict) {
+    words = transcript.split(" ")
+    for (i = 0; i < words.length; i++) {
+        if (words[i] in newDict) { words[i] = newDict[words[i]] }
+        else if (!isNaN(Number(words[i]))) { }
+        else if (words[i].length == 1) { }
+        else { words[i] = '' }
     }
-    return transcript
+    return words.join('')
 }
-word = '1'
-console.log(Number(word))
-console.log(!isNaN(Number(word)))
-transcript = 'plus plus plus minus equals to times times divide hi hello die open bracket close bracket'
-newTranscript = speechReplace(transcript, dictionaries)
-// keys = Object.keys(symbols)
-// document.querySelector('p').textContent += transcript + ' ' + transcript.replaceAll(key, symbols[key])
-document.querySelector('p').textContent += transcript + ' ' + newTranscript
+
+// List of dictionaries
+dictionaries = [symbols, brackets, greeks]
+newDict = mergeDict(dictionaries)
+
+// function speechReplace(transcript, dictionaries) {
+//     // Iterate through different dictionaries in the dictionary list
+//     for (i = 0; i < dictionaries.length; i++) {
+//         dictName = dictionaries[i]
+//         keys = Object.keys(dictName)
+//         for (j = 0; j < keys.length; j++) {
+//             transcript = transcript.replaceAll(keys[j], dictName[keys[j]])
+//         }
+//     }
+//     return transcript
+// }
+
+// word = '1'
+// console.log(Number(word))
+// console.log(!isNaN(Number(word)))
+// transcript = 'plus plus plus minus equals to times times divide hi hello die open bracket close bracket'
+// newTranscript = speechReplace(transcript, newDict)
+// // keys = Object.keys(symbols)
+// // document.querySelector('p').textContent += transcript + ' ' + transcript.replaceAll(key, symbols[key])
+// document.querySelector('p').textContent += transcript + ' ' + newTranscript
